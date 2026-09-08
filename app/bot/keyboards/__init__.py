e22257e137bd76de5c6c876fa684f0db15862edc
flaimes313-source <flaimes313@gmal.com>
2026-09-08 17:37:12 +0300

    ss

--- app/bot/keyboards/__init__.py
+++ app/bot/keyboards/__init__.py
@@ -43,6 +43,7 @@ from .diary import (
 from .dynamics import (
     get_dynamics_period_keyboard,
     get_dynamics_actions_keyboard,
+    get_dynamics_cancel_keyboard,
 )
 from .reminders import (
     get_reminders_menu_keyboard,
@@ -50,10 +51,49 @@ from .reminders import (
     get_days_keyboard,
     get_cancel_keyboard as get_reminder_cancel_keyboard,
 )
-from .pro import (  # ← ИСПРАВЛЕНО
+from .pro import (
     get_pro_menu_keyboard,
     get_pro_features_keyboard,
     get_pro_payment_keyboard,
     get_pro_success_keyboard,
     get_pro_locked_keyboard,
+    get_pro_menu_keyboard_with_back_to_profile,
+)
+from .profile import (
+    get_profile_menu_keyboard,
+    get_profile_back_keyboard,
+)
+from .support import (
+    get_support_menu_keyboard,
+    get_support_cancel_keyboard,
+)
+from .symptom_choice import (
+    get_symptom_categories_keyboard,
+    get_symptoms_by_category_keyboard,
+    get_symptom_choice_back_keyboard,
+)
+from .quick_start import (
+    get_quick_start_keyboard,
+    get_quick_start_cancel_keyboard,
+)
+from .surveys import (
+    get_survey_cancel_keyboard,
+    get_survey_skip_keyboard,
+    get_survey_finish_keyboard,
+    get_morning_question_1_keyboard,
+    get_morning_question_2_keyboard,
+    get_morning_question_3_keyboard,
+    get_morning_question_4_keyboard,
+    get_morning_question_5_keyboard,
+    get_morning_reminder_keyboard,
+    get_day_question_1_keyboard,
+    get_day_question_2_keyboard,
+    get_day_question_3_keyboard,
+    get_day_finish_keyboard,
+    get_evening_question_1_keyboard,
+    get_evening_question_2_keyboard,
+    get_evening_question_3_keyboard,
+    get_evening_question_4_keyboard,
+    get_evening_question_5_keyboard,
+    get_evening_reminder_keyboard,
 )
